58af81d241c162739be9d58aa441a3f5890fc5cd
Jorz Jayme <131635241+jijayme@users.noreply.github.com>
2025-03-06 13:50:36 +0800

    updated test.php

--- test.php
+++ test.php
@@ -6,15 +6,12 @@ $datediff = $now - $your_date;
 
 echo round($datediff / (60 * 60 * 24));
 
-
-
+ 
 
 echo "<hr/>";
-
-
-echo stripos(strtoupper("WALK IN/APPLICANT BCC-CAT"), 'WALK');
-
-
+ 
+  
+echo stripos(strtoupper("WALK IN/APPLICANT BCC-CAT"), 'WALK'); 
 
 $string = "WALK IN/APPLICANT BCC-CAT";
 $character = "WALK";
